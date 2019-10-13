efd0925967874f1d7e721bfad660f90e140422a3
muffinchucker <54907556+muffinchucker@users.noreply.github.com>
2019-10-12 20:11:54 -0400

    Collission?

---
+++ dcds/Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    private int level = 3;                                  //Current level number, expressed in game as "Day 1".
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+
+        //Call the InitGame function to initialize the first level 
+        //InitGame();
+
+    }
+
+    //Initializes the game for each level.
+   
+
+
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
+
---
+++ dcds/Assets/Scripts/Interact.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+    public class Interact : MonoBehaviour
+    {
+    List<Item> iObjects = new List<Item>();
+    Rigidbody pObject;
+    List<Item> allObjects = new List<Item>();
+    float dist;
+    SphereCollider interactRadius;
+
+    void Awake()
+    {
+        interactRadius = gameObject.AddComponent(typeof(SphereCollider)) as SphereCollider;
+        interactRadius.radius = 2.5f;
+    }
+
+    void Start()
+    {
+        
+        foreach(Item xObject in allObjects)
+        {
+            if(xObject.IsActive == true)
+            {
+                iObjects.Add(xObject);
+            }
+        }
+
+        foreach(Item interactObject in iObjects)
+        {
+            ObjectInteract(interactObject.ItemObject);
+        }
+
+    }
+
+
+    void fixedUpdate()
+    {
+        foreach (Item interactObject in iObjects)
+        {
+            ObjectInteract(interactObject.ItemObject);
+        }
+    }
+
+
+    void ObjectInteract(Rigidbody iObject)
+        {
+        dist = Vector3.Distance(iObject.position, pObject.position);
+
+        if (dist < 5.0f)
+        {
+            Console.WriteLine("Interact(E)");
+            if (Input.GetKey(KeyCode.E) == true)
+            {
+                Console.Write("You have interacted with me!");
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Console.WriteLine("You can interact with me.");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Console.WriteLine("Object out of range.");
+        }
+    }
+
+}
+
