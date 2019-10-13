a590a25cc9732e19d07d51a84a61f9a829688cfb
Tori Rossini (RIT Student) <tar6248@ad.rit.edu>
2019-10-13 12:11:11 -0400

    Rooms Spawning

---
+++ dcds/Assets/Scripts/DungeonGen/DungeonLayoutManager.cs
@@ -0,0 +1,55 @@
+﻿using Assets.Scripts.DungeonGen;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonLayoutManager : MonoBehaviour
+{
+    public GameObject roomsObject;
+    public GameObject baseCube;
+
+    List<DungeonRoom> dungeonRooms = new List<DungeonRoom>();
+    const int MIN_NUM_OF_ROOMS = 3;
+    const int MAX_NUM_OF_ROOMS = 20;
+
+    [SerializeField]
+    int DungeonHeight = 50;
+    [SerializeField]
+    int DungeonWidth = 50;
+
+    public GameObject BaseCube { get => baseCube; set => baseCube = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRooms(20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void GenerateRooms(int numRooms)
+    {
+        if(numRooms > MAX_NUM_OF_ROOMS)
+        {
+            numRooms = MAX_NUM_OF_ROOMS;
+        }
+        if (numRooms < MIN_NUM_OF_ROOMS)
+        {
+            numRooms = MIN_NUM_OF_ROOMS;
+        }
+        for( int i = 0; i < numRooms; i++)
+        {
+            dungeonRooms.Add(new DungeonRoom(RandomLocationInBox(DungeonWidth, DungeonHeight), baseCube));
+            dungeonRooms[dungeonRooms.Count - 1].RoomCube.transform.SetParent(roomsObject.transform);
+        }
+    }
+
+    private Vector3 RandomLocationInBox(int widthBox, int heightBox)
+    {
+        return new Vector3(Random.Range(0, widthBox/2), 0, Random.Range(0, heightBox/2));
+    }
+}
---
+++ dcds/Assets/Scripts/DungeonGen/DungeonRoom.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.DungeonGen
+{
+    class DungeonRoom
+    {
+        const int MAX_HEIGHT = 15;
+        const int MAX_WIDTH = 15;
+        const int MIN_HEIGHT = 5;
+        const int MIN_WIDTH = 5;
+        int width;
+        int height;
+
+        [SerializeField]
+        private GameObject roomCube;
+
+        private Rigidbody roomRigidBody;
+        private SphereCollider sphereCollider;
+
+        public GameObject RoomCube { get => roomCube; set => roomCube = value; }
+
+        public DungeonRoom(GameObject baseCube)
+        {
+            width = Random.Range(MIN_WIDTH, MAX_WIDTH);
+            height = Random.Range(MIN_HEIGHT, MAX_HEIGHT);
+            Vector3 location = Vector3.zero;
+            roomCube = GameObject.Instantiate(baseCube, location, Quaternion.identity);
+            roomCube.transform.localScale = new Vector3(width, 1, height);
+            roomRigidBody = roomCube.AddComponent<Rigidbody>();
+            roomRigidBody.useGravity = false;
+            roomRigidBody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
+            roomRigidBody.drag = 5;
+
+        }
+
+        public DungeonRoom(Vector3 loc, GameObject baseCube)
+        {
+            width = Random.Range(MIN_WIDTH, MAX_WIDTH);
+            height = Random.Range(MIN_HEIGHT, MAX_HEIGHT);
+            Vector3 location = loc;
+
+            roomCube = GameObject.Instantiate(baseCube, location, Quaternion.identity) as GameObject;
+            roomCube.transform.localScale = new Vector3(width, 1, height);
+            roomRigidBody = roomCube.AddComponent<Rigidbody>();
+            roomRigidBody.useGravity = false;
+            roomRigidBody.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
+            sphereCollider = roomCube.AddComponent<SphereCollider>();
+            sphereCollider.radius = sphereCollider.radius * 2;
+            roomRigidBody.drag = 50f;
+            roomRigidBody.mass = 20f;
+        }
+
+    }
+}
