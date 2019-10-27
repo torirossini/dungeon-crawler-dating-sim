ce81ca57329f826c828e000e7ec07cd742bc02d8
Tori Rossini (RIT Student) <tar6248@ad.rit.edu>
2019-10-27 14:33:05 -0400

    Good Camera!

--- dcds/Assets/Scripts/PlayerFollowCamera.cs
+++ dcds/Assets/Scripts/PlayerFollowCamera.cs
@@ -4,32 +4,87 @@ using UnityEngine;
 
 namespace Assets
 {
+    public enum CurrentView
+    {
+        Forward = 0,
+        Right = 1,
+        Left = 2,
+    }
+
     public class PlayerFollowCamera : MonoBehaviour
     {
         public GameObject playerObject;
-        public float cameraOffsetX = 5.0F;
-        public float cameraOffsetY = 5.0f;
-        public float cameraOffsetZ = -6.0F;
+        public CurrentView currentlyFacing;
 
-        float smoothTime = .03f;
+        public Vector3 rightCameraOffset;
+        public Vector3 centerCameraOffset;
+        public Vector3 leftCameraOffset;
+        public Quaternion defaultCameraRotation;
+        public Quaternion centerCameraRotation;
+
+        private Vector3 currentPositionOffset;
+        private Quaternion currentRotationOffset;
+
+
+        float positionSmoothTime = .03f;
+        float rotationSmoothTime = 1f;
 
         float newPosY, newPosZ, newPosX;
+        Quaternion newRot;
 
-
+        bool isTransitioning;
+        
+        void Awake()
+        {
+            
+        }
 
         void Start()
         {
-            smoothTime = playerObject.GetComponent<Player>().PlayerSpeed * Time.deltaTime;
+            positionSmoothTime = playerObject.GetComponent<Player>().PlayerSpeed * Time.deltaTime;
+            currentPositionOffset = rightCameraOffset;
+            currentRotationOffset = defaultCameraRotation;
 
         }
 
         void FixedUpdate()
         {
-            newPosZ = Mathf.Lerp(gameObject.transform.position.z, playerObject.transform.position.z + cameraOffsetZ, smoothTime);
-            newPosX = Mathf.Lerp(gameObject.transform.position.x, playerObject.transform.position.x + cameraOffsetX, smoothTime);
-            newPosY = Mathf.Lerp(gameObject.transform.position.y, playerObject.transform.position.y + cameraOffsetY, smoothTime);
 
+            switch(currentlyFacing)
+            {
+                case CurrentView.Forward:
+                    currentPositionOffset = centerCameraOffset;
+                    currentRotationOffset = centerCameraRotation;
+                    break;
+
+                case CurrentView.Right:
+                    currentPositionOffset = rightCameraOffset;
+                    currentRotationOffset = defaultCameraRotation;
+                    break;
+
+                case CurrentView.Left:
+                    currentPositionOffset = leftCameraOffset;
+                    currentRotationOffset = defaultCameraRotation;
+                    break;
+            }
+
+            newPosZ = Mathf.Lerp(gameObject.transform.position.z, playerObject.transform.position.z + currentPositionOffset.z, positionSmoothTime);
+            newPosX = Mathf.Lerp(gameObject.transform.position.x, playerObject.transform.position.x + currentPositionOffset.x, positionSmoothTime);
+            newPosY = Mathf.Lerp(gameObject.transform.position.y, playerObject.transform.position.y + currentPositionOffset.y, positionSmoothTime);
+           
             gameObject.transform.position = new Vector3(newPosX, newPosY, newPosZ);
+
+            Vector3 lookPos = playerObject.transform.position - gameObject.transform.position;
+            lookPos.y = currentRotationOffset.y;
+            newRot = Quaternion.LookRotation(lookPos);
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRot, rotationSmoothTime);
+           
+        }
+
+        void Rotate()
+        {
+
         }
     }
 }
