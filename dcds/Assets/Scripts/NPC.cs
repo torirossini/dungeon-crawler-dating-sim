f2c4ce0060818b1cfd9212b96ac905a87844d241
Tori Rossini (RIT Student) <tar6248@ad.rit.edu>
2019-11-20 19:27:20 -0500

    npc follow script

---
+++ dcds/Assets/Scripts/NPC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts
+{
+    class NPC:MonoBehaviour
+    {
+        float movementSpeed = 5f;
+
+        //Transform that NPC has to follow
+        public Transform transformToFollow;
+        //NavMesh Agent variable
+        NavMeshAgent agent;
+
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Follow the player
+            agent.destination = transformToFollow.position;
+        }
+
+        public void Move()
+        {
+            Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+
+            gameObject.transform.position += movement * movementSpeed * Time.deltaTime;
+        }
+    }
+}
