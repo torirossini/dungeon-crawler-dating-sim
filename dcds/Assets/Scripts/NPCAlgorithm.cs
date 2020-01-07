54c94a5fd95ede4db6399aa6f54325dc8554cdd7
dantayy <nvm6501@rit.edu>
2020-01-07 16:37:40 -0500

    Wrote outlines for the AI algorithm script

--- dcds/Assets/Scripts/NPCAlgorithm.cs
+++ dcds/Assets/Scripts/NPCAlgorithm.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class NPCAlgorithm : MonoBehaviour
 {
+    // When building dialog trees, have it look at a variable here representing this AI's stance/mood to determine the way the AI will talk to the player
+    public int stance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class NPCAlgorithm : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    // Helper function for rolling the Mood Of The Day modifier if it hasn't been rolled for today yet
+    public void MoodOfTheDay()
+    {
+
+    }
+
+    // Helper function for calculating stance change based on player input during dialog goes here
+    public void StanceChange()
+    {
+
     }
 }
