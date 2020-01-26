6d1b20d230e6ea25e40754331d37f3dd9494df99
Dantayy <nvm6501@rit.edu>
2020-01-26 14:44:54 -0500

    Added basic inventory management script and began to implement UI features to reflect its implementation

---
+++ dcds/Assets/Scripts/Item Scripts/PlayerInventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    // very basic inventory backend that allows items to be added/removed from a list
+    public class PlayerInventory : MonoBehaviour
+    {
+        public int inventorySize = 4;
+        public List<Item> inventory;
+
+        public void AddItem(Item itemToAdd)
+        {
+            if (inventory.Count < inventorySize)
+                inventory.Add(itemToAdd);
+            else
+                Debug.Log("Inventory is full, can't add this item!");
+        }
+        public void RemoveItem(Item itemToRemove)
+        {
+            inventory.Remove(itemToRemove);
+        }
+    }
+}
