9b8b874a7fb63c152a781b045152a5050c58475a
Nora Murren (RIT Student) <bsm4978@ad.rit.edu>
2020-02-17 16:45:26 -0500

    Added LoadVariables functionality
    - added fungus command to load mood variables into fungus flowchart
    - test flowchart updated to test functionality

---
+++ dcds/Assets/Scripts/LoadVariables.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+namespace Assets.Scripts
+{
+    [CommandInfo("Custom", "LoadVariables", "Loads external variables")]
+    [AddComponentMenu("")]
+    public class LoadVariables : Command
+    {
+        [SerializeField] protected NPC thisNPC;
+
+        [Tooltip("Current Mood enum (Good 0, Neutral 1, Bad 2)")]
+        [VariableProperty(typeof(IntegerVariable))]
+        [SerializeField] protected IntegerVariable mood;
+
+        [Tooltip("Stance scale from -20 to 20")]
+        [VariableProperty(typeof(IntegerVariable))]
+        [SerializeField] protected IntegerVariable stance;
+
+        public override void OnEnter()
+        {
+            thisNPC = gameObject.GetComponentInParent<NPC>();
+
+            if (mood != null)
+            {
+                mood.Value = (int)thisNPC.currentMood;
+            }
+
+            if (stance != null)
+            {
+                stance.Value = thisNPC.stance;
+            }
+
+            Continue();
+        }
+    }
+}
