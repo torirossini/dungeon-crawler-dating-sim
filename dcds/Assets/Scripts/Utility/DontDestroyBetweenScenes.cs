e3fe0d575d4a82e0e0834d7914f265486a06814e
dantayy <nvm6501@rit.edu>
2020-02-20 17:31:57 -0500

    Great googly moogly it's all gone horribly wrong!  The canvas is now carried between scenes properly (needed an Event System obj), but the code that does it now doesn't work at all for the player, failing to even log debug lines.  Will look into this more soon tm

--- dcds/Assets/Scripts/Utility/DontDestroyBetweenScenes.cs
+++ dcds/Assets/Scripts/Utility/DontDestroyBetweenScenes.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 // helper script for keeping a GO persistant between scenes
 public class DontDestroyBetweenScenes : MonoBehaviour
 {
-    private void Awake()
+    private void Start()
     {
+        Debug.Log("IM ALIVE!");
         // check for multiple instances of obj, Destroy if this already exists, otherwise add it to DonDestroyOnLoad group
         // GO NEEDS TO BE TAGGED FOR THIS TO WORK
         int numInstances = GameObject.FindGameObjectsWithTag(gameObject.tag).Length;
         if (numInstances > 1)
-            Destroy(gameObject);
+        {
+            Debug.Log("Destroying duplicate of " + gameObject.tag);
+            //Destroy(gameObject);
+        }
         else
+        {
+            Debug.Log("Adding " + gameObject.tag + " to DontDestroyOnLoad");
             DontDestroyOnLoad(gameObject);
+        }
     }
 }
