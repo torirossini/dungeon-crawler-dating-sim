942d2199718cb8e3a7b7fd41547e06b202b3bcd7
Nora Murren (RIT Student) <bsm4978@ad.rit.edu>
2020-02-23 13:23:08 -0500

    added stance changing functionality
    - test flowchart pulls in stance and mood variables, changes them based on dialogue, then saves them back into the NPC script
    - made a separate folder for fungus utility scripts

---
+++ dcds/Assets/Scripts/Fungus Utility/SaveVariables.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+namespace Assets.Scripts
+{
+    [CommandInfo("Custom", "SaveVariables", "Saves modified variables back into NPC script")]
+    [AddComponentMenu("")]
+    public class SaveVariables : Command
+    {
+        [SerializeField] protected NPC thisNPC;
+
+        [Tooltip("Current Mood enum (Good 0, Neutral 1, Bad 2)")]
+        [VariableProperty(typeof(IntegerVariable))]
+        [SerializeField] protected IntegerVariable mood;
+
+        [Tooltip("Stance scale from -20 to 20")]
+        [VariableProperty(typeof(IntegerVariable))]
+        [SerializeField] protected IntegerVariable stance;
+
+        public override void OnEnter()
+        {
+            thisNPC = gameObject.GetComponentInParent<NPC>();
+
+            if (mood != null)
+            {
+                thisNPC.currentMood = (NPC.Mood)mood.Value;
+            }
+
+            if (stance != null)
+            {
+                thisNPC.stance = stance.Value;
+            }
+
+            Continue();
+        }
+    }
+}
