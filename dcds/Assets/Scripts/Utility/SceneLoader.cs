3c39a4ec115d1444abbe4bd8319dcdd1336d8ae8
Tori Rossini <tori.rossini42@gmail.com>
2020-05-01 19:30:01 -0400

    Title Screen

---
+++ dcds/Assets/Scripts/Utility/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    int SceneToLoad = 1;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
